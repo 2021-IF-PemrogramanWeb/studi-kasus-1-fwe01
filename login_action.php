665e94e34bd0efc6001fc3ebae808532b2572c47
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-12-01 18:48:52 +0700

    Fix : ensure alphanumeric

--- login_action.php
+++ login_action.php
@@ -8,26 +8,29 @@ session_destroy();
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
-$statement = $conn->prepare(
-	"
+if (ctype_alnum($username)) {
+	$statement = $conn->prepare(
+		"
 	select *
 	from users
 	where BINARY username = '" . $username . "'
 	limit 1;
 "
-);
+	);
 
-$statement->execute();
-$statement->setFetchMode(PDO::FETCH_ASSOC);
-$results = $statement->fetchAll();
+	$statement->execute();
+	$statement->setFetchMode(PDO::FETCH_ASSOC);
+	$results = $statement->fetchAll();
 
-session_start();
-if (md5($password) == $results[0]['password']) {
-	$_SESSION['auth_username'] = $results[0]['username'];
-	$_SESSION['auth_name'] = $results[0]['name'];
-	header("Location: http://pweb.frederickwilliame.com/loggedin.php");
-}
-else {
+	session_start();
+	if (md5($password) == $results[0]['password']) {
+		$_SESSION['auth_username'] = $results[0]['username'];
+		$_SESSION['auth_name'] = $results[0]['name'];
+		header("Location: http://pweb.frederickwilliame.com/loggedin.php");
+	} else {
+		echo 'login failed';
+	}
+} else {
 	echo 'login failed';
 }
 exit();
